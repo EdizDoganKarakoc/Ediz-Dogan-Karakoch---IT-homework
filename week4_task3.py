eeaf7c92973e3b031e31996d3870abc96331828f
EdizDoganKarakoc <karakocedizdogan@gmail.com>
2025-10-03 15:10:33 +0300

    Updated task3 week4

--- week4_task3.py
+++ week4_task3.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 x = ['Лекции', 'Семинары']
 y = [99, 0.1]
 
-plt.bar(x, y, label = 'Данные с календаря', color = "purple", alpha = 0.6)
+plt.bar(x, y, label = 'Информатика', color = "purple", alpha = 0.6)
 
 
-plt.xlabel('Месяц года')
-plt.ylabel('Кол-во дней')
+plt.xlabel('Процент материала который я понимаю')
+plt.ylabel('%')
 plt.title('Столбчатая диаграмма')
 plt.legend(loc='center')
 plt.show()

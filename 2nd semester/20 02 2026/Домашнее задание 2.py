854c5139c47a9f328f8d6e0db6013f70ed85a171
Ediz Dogan Karakoc <karakocedizdogan@gmail.com>
2026-02-20 03:24:40 +0300

    3rd homework push

--- 2nd semester/20 02 2026/Домашнее задание 2.py
+++ 2nd semester/20 02 2026/Домашнее задание 2.py
@@ -11,3 +11,33 @@ G.add_edges_from(edges)
 plt.figure(figsize=(5, 5))
 nx.draw(G,  with_labels=True, node_color='lightgreen', edge_color='black', width=3, arrows=True)
 plt.show()
+
+
+
+def find_path(edges, start_node, end_node, visited = []):
+    if start_node in visited:
+        return
+
+    elif start_node == end_node:
+        print(f"Path successful!")
+        global path_counter
+        path_counter += 1
+        return
+
+    else:
+        for i in edges:
+            if i[0] == start_node:
+                find_path(edges, i[1], end_node, visited + [start_node])
+
+path_counter = 0
+find_path(edges, "A", "C")
+a_c = path_counter
+print(f"There are {a_c} ways to get from 'A' to 'C'.")
+
+path_counter = 0
+find_path(edges, "C", "I")
+c_i = path_counter
+print(f"There are {c_i} ways to get from 'C' to 'I'.")
+
+
+print(f"Eventually, there are {a_c * c_i} ways to get from 'A' to 'I', having have visited C.")
